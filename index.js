eb20d7ef539003078451b3d3d7f51ab4d7425fab
Chase Nicks <chasenicks81@gmail.com>
2021-08-29 21:55:54 -0400

    started writing functions for each prompt command

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const mysql  = require("mysql2");
+const mysql = require("mysql2");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
 
-const db_connection  = mysql.createConnection( 
+const db_connection = mysql.createConnection(
 
     {
-        host:"localhost",
-        port:"3306",
-        user:"root",
-        password:"Reddog99",
-        database:"company_db",
+        host: "localhost",
+        port: "3306",
+        user: "root",
+        password: "Reddog99",
+        database: "company_db",
     },
     console.log("Connected to company_db")
 );
@@ -20,56 +20,56 @@ db_connection.connect(function (err) {
 });
 
 const start = () => {
-  inquirer
-  .prompt ([
-      {
-          type: "list",
-          message: "What would you like to do?",
-          name: "choice",
-          choices: [
-              "View all departments",
-              "View all roles",
-              "View all employees",
-              "Add a department",
-              "Add a role",
-              "Add a employee",
-              "Update an employee role"
-          ],
-      },
-  ])
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "What would you like to do?",
+                name: "choice",
+                choices: [
+                    "View all departments",
+                    "View all roles",
+                    "View all employees",
+                    "Add a department",
+                    "Add a role",
+                    "Add a employee",
+                    "Update an employee role"
+                ],
+            },
+        ])
 
-  .then((val) => {
-      switch (val.choices) {
+        .then(function(val) {
+            switch (val.choice) {
 
-        case "View all departments":
-            viewAllDepartments();
-            break;
+                case "View all departments":
+                    viewAllDepartments();
+                    break;
 
-        case "View all roles?":
-            viewAllRoles();
-            break;
+                case "View all roles?":
+                    viewAllRoles();
+                    break;
 
-        case "View all employees?":
-            viewAllEmployees();
-            break;
+                case "View all employees?":
+                    viewAllEmployees();
+                    break;
 
-        case "Add a department?":
-            addAdeparment();
-            break;
+                case "Add a department?":
+                    addAdeparment();
+                    break;
 
-        case "Add a role":
-            addArole();
-            break;
-        
-        case "Add a emoployee":
-            addAemployee();
-            break;
+                case "Add a role":
+                    addArole();
+                    break;
 
-        case "Update an employee role?":
-            updateEmployeeRole();
-            break;
-      }
-  });
+                case "Add a emoployee":
+                    addAemployee();
+                    break;
+
+                case "Update an employee role?":
+                    updateEmployeeRole();
+                    break;
+            }
+        });
 };
 
 const viewAllDepartments = () => {
@@ -77,7 +77,7 @@ const viewAllDepartments = () => {
     const query = "SELECT * FROM department;";
     db_connection.query(query,
 
-        function(err, res) {
+        function (err, res) {
             if (err) throw err
             console.table(res);
             start();
@@ -85,25 +85,87 @@ const viewAllDepartments = () => {
 };
 
 const viewAllRoles = () => {
-  
+
+    const query = "";
+    db_connection.query(query,
+
+        function (err, res) {
+            if (err) throw err
+            console.table(res);
+            start();
+        });
+
 }
 
 const viewAllEmployees = () => {
-  
+
+    const query = "";
+    db_connection.query(query,
+
+        function (err, res) {
+            if (err) throw err
+            console.table(res);
+            start();
+        });
+
 }
 
 const addAdeparment = () => {
-  
+
+    inquirer.prompt([
+       {
+           type:"",
+           message:"",
+           name:"",
+       } 
+    ])
+
+    const query = "";
+    db_connection.query(query,
+
+        function (err, res) {
+            if (err) throw err
+            console.table(res);
+            start();
+        });
+
 }
 
 const addArole = () => {
-  
+
+    const query = "INSERT INTO role (id, title, salary, department_id) VALUES ()";
+    db_connection.query(query,
+
+        function (err, res) {
+            if (err) throw err
+            console.table(res);
+            start();
+        });
+
 }
 
 const addAemployee = () => {
-  
+
+    const query = "INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES ()";
+    db_connection.query(query,
+
+        function (err, res) {
+            if (err) throw err
+            console.table(res);
+            start();
+        });
+
 }
 
 const updateEmployeeRole = () => {
-  
+
+    const query = "UPDATE employee SET column1 = value1, column2 = value2 WHERE ";
+    db_connection.query(query,
+
+        function (err, res) {
+            if (err) throw err
+            console.table(res);
+            start();
+        });
+
 }
